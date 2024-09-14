2a88237c682777176db35c88287d19024f335948
AshkanArabim <ashkan.arabim@gmail.com>
2024-09-14 14:15:47 -0600

    changed word2ipa to use requests library

--- python-scripts/turnWordToIpa.py
+++ python-scripts/turnWordToIpa.py
@@ -1,13 +1,9 @@
-import unirest
+from requests.auth import HTTPBasicAuth
+import requests
 import os
 
-api_key = "<" + os.environ.get('WORDS_API_KEY') + ">"
-                         
-def word2Ipa():
-  # Authorization for WordsAPI
-  response = unirest.get("https://wordsapiv1.p.mashape.com/words/soliloquy",
-    headers={
-      "X-Mashape-Key": api_key,
-      "Accept": "application/json"
-    }
-  )
+def word2Ipa(word):
+  api_key = "<" + os.environ.get('WORDS_API_KEY') + ">"
+  auth = HTTPBasicAuth('apikey', '62661a091cmshd7f7e3dac296219p1918f4jsn2a253a5c6f6e')
+  response = requests.get("https://wordsapiv1.p.rapidapi.com/words/" + word + "/pronunciation", auth=auth)
+  return response.text
