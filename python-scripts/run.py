dc06834160460778d8c133ed12c1e45ec87d1352
Javer <jvazquez073015@gmail.con>
2024-09-14 13:14:33 -0600

    Add Audio to IPA functionality

--- python-scripts/run.py
+++ python-scripts/run.py
@@ -1,10 +1,15 @@
 from flask import Flask
+import aud2ipa
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "home"
+
+@app.route("/aud2ipa")
+def aud2ipa():
+    return "ipa set"
 
 if __name__ == "__main__":
     app.run(port=5000)
