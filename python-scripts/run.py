0379d38d21e716c3845149f1fd3ab1507b298f95
TadeoDelaRocha <tdelarochagalan@gmail.com>
2024-09-14 13:12:35 -0600

    Added Word to api call

--- python-scripts/run.py
+++ python-scripts/run.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from turnWordToIpa import word2Ipa
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "home"
+
+@app.route('/word2ipa/<word>')
+def word2ipa(word):
+    return word2Ipa(word)
 
 if __name__ == "__main__":
     app.run(port=5000)
