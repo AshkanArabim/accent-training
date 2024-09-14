b152412aa3c401697c62237f321ba4b26d827e51
AshkanArabim <ashkan.arabim@gmail.com>
2024-09-14 14:55:14 -0600

    Fixed turnWordToIpa

--- python-scripts/turnWordToIpa.py
+++ python-scripts/turnWordToIpa.py
@@ -1,9 +1,18 @@
-from requests.auth import HTTPBasicAuth
-import requests
 import os
+import requests
+import json
 
 def word2Ipa(word):
-  api_key = "<" + os.environ.get('WORDS_API_KEY') + ">"
-  auth = HTTPBasicAuth('apikey', '62661a091cmshd7f7e3dac296219p1918f4jsn2a253a5c6f6e')
-  response = requests.get("https://wordsapiv1.p.rapidapi.com/words/" + word + "/pronunciation", auth=auth)
-  return response.text
+  api_key = os.environ.get('WORDS_API_KEY')
+  url = "https://wordsapiv1.p.rapidapi.com/words/" + word + "/pronunciation"
+
+  headers = {
+    "x-rapidapi-key": api_key,
+    "x-rapidapi-host": "wordsapiv1.p.rapidapi.com"
+  }
+
+  response = requests.get(url, headers=headers)
+
+  response = response.json()
+
+  return response['pronunciation']['all']
