72a24fe9f6bfda8f48f0b4a8f01b4c13aacad502
Javer <jvazquez073015@gmail.con>
2024-09-14 18:44:40 -0600

    Set up Gemini Call

--- frontend/my-app/src/index.js
+++ frontend/my-app/src/index.js
@@ -4,6 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// const { GoogleGenerativeAI } = require("@google/generative-ai"); TODO: Move to another place
+// // Make sure to include these imports:
+// // import { GoogleGenerativeAI } from "@google/generative-ai";
+// const genAI = new GoogleGenerativeAI(process.env.REACT_APP_API_KEY);
+// const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+// const prompt = "Write a story about a magic backpack.";
+
+// const result = await model.generateContent(prompt);
+// console.log(result.response.text());
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
